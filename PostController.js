28aa38ce050a15ffb8ce5d91e8e6d1d65dad72f4
2vasy <2vasy@mail.ru>
2025-01-30 10:23:14 +0100

    add_middleware

--- PostController.js
+++ PostController.js
@@ -1,57 +1,34 @@
 import Post from "./Post.js";
 
 export const getAll = async (req, res) => {
-  try {
-    const posts = await Post.find();
-    res.json(posts);
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
-  }
+  const posts = await Post.find();
+  if (!posts) throw new Error("No post found");
+  res.json(posts);
 };
 
 export const getOne = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const post = await Post.findById(id);
-    if (!post) return res.status(404).json({ message: "Post not found" });
-    res.json(post);
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
-  }
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  if (!post) throw new Error("Post not found");
+  res.json(post);
 };
 
 export const create = async (req, res) => {
-  try {
-    const post = new Post(req.body);
-    await post.save();
-    res.json(post);
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
-  }
+  const post = new Post(req.body);
+  await post.save();
+  res.json(post);
 };
 
 export const update = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedPost = await Post.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    if (!updatedPost)
-      return res.status(404).json({ message: "Post not found" });
-    res.json(updatedPost);
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
-  }
+  const { id } = req.params;
+  const updatedPost = await Post.findByIdAndUpdate(id, req.body, { new: true });
+  if (!updatedPost) throw new Error("Post not found");
+  res.json(updatedPost);
 };
 
 export const deletePost = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedPost = await Post.findByIdAndDelete(id);
-    if (!deletedPost)
-      return res.status(404).json({ message: "Post not found" });
-    res.json({ message: "Post deleted successfully!" });
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
-  }
+  const { id } = req.params;
+  const deletedPost = await Post.findByIdAndDelete(id);
+  if (!deletedPost) throw new Error("Post not found");
+  res.json({ message: "Post deleted successfully!" });
 };
